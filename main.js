43f7518e03bc68d2742a1582b548959af8009341
Bekzod Developer <bkzd18kholdarov@gmail.com>
2022-10-29 15:39:02 +0500

    Find the Length of a String

--- main.js
+++ main.js
@@ -4,10 +4,18 @@
 // const myName = "Bekzod";
 // const myStr = "Hello, my name is " + myName + ", how are you?";
 
-const someAdjective = "cool!";
-let myStr = "Learning to code is ";
+// const someAdjective = "cool!";
+// let myStr = "Learning to code is ";
 
-myStr += someAdjective;
+// myStr += someAdjective;
 
-console.log(myStr);
+// console.log(myStr);
+
+
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
 
