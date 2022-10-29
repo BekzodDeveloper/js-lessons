6a65dfae199215fbfcfe9ea7fcdd1f628da748fd
Bekzod Developer <bkzd18kholdarov@gmail.com>
2022-10-29 15:37:23 +0500

    Appending Variables to Strings

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 // let myStr = "This is the first sentence. ";
 // myStr += "This is the second sentence.";
 
-const myName = "Bekzod";
-const myStr = "Hello, my name is " + myName + ", how are you?";
+// const myName = "Bekzod";
+// const myStr = "Hello, my name is " + myName + ", how are you?";
+
+const someAdjective = "cool!";
+let myStr = "Learning to code is ";
+
+myStr += someAdjective;
 
 console.log(myStr);
 
