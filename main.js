33d19223388eed3b10f0fc9366604a1fccd87970
Bekzod Developer <bkzd18kholdarov@gmail.com>
2022-10-29 15:35:49 +0500

    Constructing Strings with Variables

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-let myStr = "This is the first sentence. ";
-myStr += "This is the second sentence.";
+// let myStr = "This is the first sentence. ";
+// myStr += "This is the second sentence.";
+
+const myName = "Bekzod";
+const myStr = "Hello, my name is " + myName + ", how are you?";
 
 console.log(myStr);
+
